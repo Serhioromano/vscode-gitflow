1e0a69e905f8ee2685b5a7cdeb5b69259cbb37c6
serhioromano <serhioromano@outlook.com>
2022-02-07 11:40:37 +0600

    finish

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -2,9 +2,9 @@ import * as vscode from "vscode";
 import {Disposable} from "./disposables";
 
 export enum LogLevels {
-    info = 'INFO',
-    warning = 'WARNING',
-    error = 'ERROR'
+    info = "INFO",
+    warning = "WARNING",
+    error = "ERROR",
 }
 
 export class Logger extends Disposable {
@@ -21,8 +21,18 @@ export class Logger extends Disposable {
     }
 
     public log(msg: string, cmd: string, level: LogLevels = LogLevels.info) {
-        const date = new Date();
-
+        if (
+            [
+                "git version",
+                "git status",
+                "git branch",
+                "git tag --sort=-v:refname",
+                "git branch -r",
+                "git flow config list",
+            ].includes(cmd)
+        ) {
+            return;
+        }
         this.channel.appendLine(`${level}: [${cmd}] ${msg}`);
     }
 }
